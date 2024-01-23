81c4b27121714d735c4f7a8a3126b1f4e3acde7c
LazyDope <mydj555@gmail.com>
2024-01-23 16:58:25 -0500

    various improvements - fix radial rules - add pausing and clearing - add drag to draw

---
+++ src/lib.rs
@@ -0,0 +1,337 @@
+use std::{
+    ops::{Index, IndexMut},
+    time::Instant,
+};
+
+use nannou::{color::DefaultScalar, prelude::*};
+
+pub struct Model<const GRID_SIZE: usize> {
+    active: Grid<GRID_SIZE>,
+    rules: Vec<Rule>,
+    last: Instant,
+    paused: bool,
+    mouse_pressed: bool,
+    last_pos: Option<Point2>,
+    drag_list: Vec<(usize, usize)>,
+}
+
+#[derive(Clone)]
+pub struct Grid<const GRID_SIZE: usize> {
+    grid: [[State; GRID_SIZE]; GRID_SIZE],
+}
+
+impl<const GRID_SIZE: usize> Grid<GRID_SIZE> {
+    fn get_cell(&self, x: usize, y: usize) -> Option<&State> {
+        self.get_col(x).map(|col| col.get(y)).flatten()
+    }
+
+    fn get_col(&self, x: usize) -> Option<&[State; GRID_SIZE]> {
+        self.grid.get(x)
+    }
+
+    fn indexed_iter(&self) -> impl Iterator<Item = (usize, usize, &State)> {
+        self.grid
+            .iter()
+            .enumerate()
+            .flat_map(|(i, col)| col.iter().enumerate().map(move |(j, cell)| (i, j, cell)))
+    }
+}
+
+impl<const GRID_SIZE: usize> From<[[State; GRID_SIZE]; GRID_SIZE]> for Grid<GRID_SIZE> {
+    fn from(value: [[State; GRID_SIZE]; GRID_SIZE]) -> Self {
+        Grid { grid: value }
+    }
+}
+
+impl<const GRID_SIZE: usize> Index<(usize, usize)> for Grid<GRID_SIZE> {
+    type Output = State;
+
+    fn index(&self, index: (usize, usize)) -> &Self::Output {
+        &self.grid[index.0][index.1]
+    }
+}
+
+impl<const GRID_SIZE: usize> IndexMut<(usize, usize)> for Grid<GRID_SIZE> {
+    fn index_mut(&mut self, index: (usize, usize)) -> &mut Self::Output {
+        &mut self.grid[index.0][index.1]
+    }
+}
+
+impl<const GRID_SIZE: usize> Default for Grid<GRID_SIZE> {
+    fn default() -> Self {
+        Self::from([[Default::default(); GRID_SIZE]; GRID_SIZE])
+    }
+}
+
+#[derive(Clone, Copy, PartialEq, Eq)]
+pub enum State {
+    Full,
+    Empty,
+}
+
+impl std::fmt::Debug for State {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            State::Full => write!(f, "X"),
+            State::Empty => write!(f, "O"),
+        }
+    }
+}
+
+pub enum Rule {
+    Linear {
+        in_state: Vec<Vec<Option<State>>>,
+        out_state: Vec<Vec<Option<State>>>,
+    },
+    Radial {
+        current_state: State,
+        surroundings: Vec<(State, Comparison<usize>)>,
+        final_state: State,
+    },
+}
+
+pub enum Comparison<T> {
+    Equal(T),
+    NotEqual(T),
+    GreaterThan(T),
+    LessThan(T),
+    GreaterThanOrEqual(T),
+    LessThanOrEqual(T),
+    BetweenExclusive(T, T),
+    BetweenInclusive(T, T),
+}
+
+impl<T: PartialOrd> Comparison<T> {
+    fn compare(&self, other: T) -> bool {
+        match self {
+            Comparison::Equal(value) => &other == value,
+            Comparison::NotEqual(value) => &other != value,
+            Comparison::GreaterThan(value) => &other > value,
+            Comparison::LessThan(value) => &other < value,
+            Comparison::GreaterThanOrEqual(value) => &other >= value,
+            Comparison::LessThanOrEqual(value) => &other <= value,
+            Comparison::BetweenExclusive(lower, upper) => lower < &other && &other < upper,
+            Comparison::BetweenInclusive(lower, upper) => lower <= &other && &other <= upper,
+        }
+    }
+}
+
+impl Default for State {
+    fn default() -> Self {
+        Self::Empty
+    }
+}
+
+impl<const GRID_SIZE: usize> Model<GRID_SIZE> {
+    pub fn model(
+        _app: &App,
+        starting_state: Option<Grid<GRID_SIZE>>,
+        rules: Vec<Rule>,
+        paused: bool,
+    ) -> Model<GRID_SIZE> {
+        let active = starting_state.unwrap_or_default();
+        Model {
+            active,
+            rules,
+            last: Instant::now(),
+            paused,
+            mouse_pressed: false,
+            last_pos: Default::default(),
+            drag_list: Default::default(),
+        }
+    }
+}
+
+pub fn event<const GRID_SIZE: usize>(app: &App, model: &mut Model<GRID_SIZE>, event: Event) {
+    match event {
+        Event::WindowEvent {
+            simple: Some(event),
+            ..
+        // clicking or tapping in a cell to swap it's 'fullness'
+        } => match event {
+            MousePressed(_) => {
+                if let Some(last_pos) =  model.last_pos.clone() {
+                    update_grid(&app.main_window().rect(), model, &last_pos);
+                }
+                model.mouse_pressed = true
+            }
+            MouseReleased(_) => {
+                model.drag_list.clear();
+                model.mouse_pressed = false;
+            },
+            Touch(touch_event) => {
+                update_grid(&app.main_window().rect(), model, &touch_event.position)
+            }
+            MouseMoved(pos) => {
+                if model.mouse_pressed {
+                    update_grid(&app.main_window().rect(), model, &pos);
+                }
+                model.last_pos = Some(pos);
+            }
+            KeyPressed(key) => {
+                match key {
+                    Key::P => model.paused = !model.paused,
+                    Key::C => model.active = Default::default(),
+                    _ => (),
+                }
+            }
+            _ => (),
+        },
+        _ => (),
+    }
+}
+
+// changes the state of the cell that was interacted with
+fn update_grid<const GRID_SIZE: usize>(win: &Rect, model: &mut Model<GRID_SIZE>, win_pos: &Point2) {
+    let w = win.x.len() / (GRID_SIZE as f32);
+    let h = win.y.len() / (GRID_SIZE as f32);
+    let x = ((win_pos.x - win.x.start) / w) as usize;
+    let y = ((win.y.end - win_pos.y) / h) as usize;
+    if !model.drag_list.contains(&(x, y)) {
+        model.active[(x, y)] = match model.active[(x, y)] {
+            State::Full => State::Empty,
+            State::Empty => State::Full,
+        };
+        model.drag_list.push((x, y));
+    }
+}
+
+pub fn update<const GRID_SIZE: usize>(_app: &App, model: &mut Model<GRID_SIZE>, _update: Update) {
+    if model.paused || model.last.elapsed().as_millis() < 50 {
+        return;
+    } else {
+        model.last = Instant::now();
+    }
+    let mut inactive = model.active.clone();
+    for rule in model.rules.iter() {
+        for (i, j, cell) in model.active.indexed_iter() {
+            if match rule {
+                Rule::Linear {
+                    in_state,
+                    out_state,
+                } => model.linear((i, j), &mut inactive, in_state, out_state),
+                Rule::Radial {
+                    current_state,
+                    surroundings,
+                    final_state,
+                } => model.radial(
+                    &cell,
+                    (i, j),
+                    &mut inactive,
+                    current_state,
+                    surroundings,
+                    final_state,
+                ),
+            } {
+                continue;
+            }
+        }
+    }
+    model.active = inactive;
+}
+
+impl<const GRID_SIZE: usize> Model<GRID_SIZE> {
+    fn radial(
+        &self,
+        cell: &State,
+        cell_cords: (usize, usize),
+        inactive: &mut Grid<GRID_SIZE>,
+        current_state: &State,
+        surroundings: &[(State, Comparison<usize>)],
+        final_state: &State,
+    ) -> bool {
+        if current_state == cell {
+            let cells: Vec<_> = (-1i64..=1)
+                .flat_map(|ri| {
+                    (-1i64..=1)
+                        .map(move |rj| (ri, rj))
+                        .filter(|r| r != &(0, 0))
+                        .map(|(ri, rj)| {
+                            self.active.get_cell(
+                                (cell_cords.0 as i64 + ri) as usize,
+                                (cell_cords.1 as i64 + rj) as usize,
+                            )
+                        })
+                })
+                .filter_map(|cell| cell)
+                .collect();
+            if surroundings.iter().all(|(req_state, comp)| {
+                comp.compare(cells.iter().filter(|val| val == &&req_state).count())
+            }) {
+                inactive[(cell_cords.0, cell_cords.1)] = final_state.clone();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    fn linear(
+        &self,
+        cell_cords: (usize, usize),
+        inactive: &mut Grid<GRID_SIZE>,
+        in_state: &[Vec<Option<State>>],
+        out_state: &[Vec<Option<State>>],
+    ) -> bool {
+        // check all the states relative to the given cell
+        if in_state
+            .iter()
+            .enumerate()
+            .flat_map(|(ri, rule_col)| {
+                rule_col
+                    .iter()
+                    .enumerate()
+                    .map(move |(rj, rule_cell)| (ri, rj, rule_cell))
+            })
+            .filter_map(|(ri, rj, rule_cell)| rule_cell.map(|cell| (ri, rj, cell)))
+            .all(|(ri, rj, rule_cell)| {
+                self.active
+                    .get_cell(cell_cords.0 + ri, cell_cords.1 + rj)
+                    .is_some_and(|cell| cell == &rule_cell)
+                    && inactive
+                        .get_cell(cell_cords.0 + ri, cell_cords.1 + rj)
+                        .is_some_and(|cell| cell == &rule_cell)
+            })
+        {
+            // if cells match expected, perform the swaps to the new layout
+            for (ri, rj, state) in out_state.iter().enumerate().flat_map(|(ri, out_col)| {
+                out_col
+                    .iter()
+                    .enumerate()
+                    .filter_map(move |(rj, state)| state.map(|state| (ri, rj, state)))
+            }) {
+                inactive[(cell_cords.0 + ri, cell_cords.1 + rj)] = state.clone();
+            }
+            return true;
+        }
+        return false;
+    }
+}
+
+pub fn view<const GRID_SIZE: usize>(app: &App, model: &Model<GRID_SIZE>, frame: Frame) {
+    let draw = app.draw();
+    let window = app.main_window();
+    let win = window.rect();
+    draw.background().color(BLUE);
+
+    draw_grid(&draw, &win, &model);
+
+    draw.to_frame(app, &frame).unwrap();
+}
+
+fn draw_grid<const GRID_SIZE: usize>(draw: &Draw, win: &Rect, model: &Model<GRID_SIZE>) {
+    let w = win.x.len() / (GRID_SIZE as f32);
+    let h = win.y.len() / (GRID_SIZE as f32);
+    let x0 = win.x.start + w / 2.;
+    let y0 = win.y.end - h / 2.;
+    for (i, j, cell) in model.active.indexed_iter() {
+        draw.rect()
+            .x_y(x0 + (i as f32) * w, y0 - (j as f32) * h)
+            .w_h(w, h)
+            .stroke_weight(0.5)
+            .stroke(GRAY)
+            .color(match cell {
+                State::Full => WHITE,
+                State::Empty => BLACK,
+            });
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,264 +1,59 @@
-use std::time::Instant;
-
-use nannou::prelude::*;
+use cellular_automaton::*;
 
 fn main() {
-    nannou::app(model)
+    nannou::app(|app| Model::<50>::model(app, None, game_of_life(), false))
         .update(update)
         .event(event)
         .simple_window(view)
         .run();
 }
 
-const GRID_SIZE: usize = 20;
-
-struct Model {
-    active: [[State; GRID_SIZE]; GRID_SIZE],
-    rules: Vec<Rule>,
-    last: Instant,
-    last_pos: Option<Point2>,
-}
-
-#[derive(Clone, Copy, PartialEq, Eq)]
-enum State {
-    Full,
-    Empty,
-}
-
-enum Rule {
-    Axial {
-        in_state: Vec<Vec<State>>,
-        out_state: Vec<Vec<State>>,
-    },
-    Radial {
-        current_state: State,
-        surroundings: Vec<(usize, State, Comparison)>,
-        final_state: State,
-    },
-}
-
-#[derive(PartialEq, Eq)]
-enum Comparison {
-    Equal,
-    NotEqual,
-    GreaterThan,
-    LessThan,
-    GreaterThanOrEqual,
-    LessThanOrEqual,
-}
-
-impl Comparison {
-    fn compare<T>(&self, lhs: T, rhs: T) -> bool
-    where
-        T: PartialOrd,
-    {
-        match lhs.partial_cmp(&rhs) {
-            Some(ord) => match ord {
-                std::cmp::Ordering::Less => {
-                    self == &Self::LessThan
-                        || self == &Self::LessThanOrEqual
-                        || self == &Self::NotEqual
-                }
-                std::cmp::Ordering::Equal => {
-                    self == &Self::Equal
-                        || self == &Self::LessThanOrEqual
-                        || self == &Self::GreaterThanOrEqual
-                }
-                std::cmp::Ordering::Greater => {
-                    self == &Self::GreaterThan
-                        || self == &Self::GreaterThanOrEqual
-                        || self == &Self::NotEqual
-                }
-            },
-            None => false,
-        }
-    }
-}
-
-impl Default for State {
-    fn default() -> Self {
-        Self::Empty
-    }
-}
-
-fn model(_app: &App) -> Model {
-    let active = [[Default::default(); GRID_SIZE]; GRID_SIZE];
-    Model {
-        active,
-        rules: vec![Rule::Axial {
+fn falling_sand() -> Vec<Rule> {
+    vec![
+        Rule::Linear {
             // if there is a full cell above an empty cell, swap them
-            in_state: vec![vec![State::Full, State::Empty]],
-            out_state: vec![vec![State::Empty, State::Full]],
-        }],
-        last: Instant::now(),
-        last_pos: Default::default(),
-    }
-}
-
-fn event(app: &App, model: &mut Model, event: Event) {
-    match event {
-        Event::WindowEvent {
-            simple: Some(event),
-            ..
-        // clicking or tapping in a cell to swap it's 'fullness'
-        } => match event {
-            MousePressed(_) if model.last_pos.is_some() => {
-                let last_pos = model.last_pos.clone().unwrap();
-                update_grid(&app.main_window().rect(), model, &last_pos)
-            }
-            Touch(touch_event) => {
-                update_grid(&app.main_window().rect(), model, &touch_event.position)
-            }
-            MouseMoved(pos) => model.last_pos = Some(pos),
-            _ => (),
+            in_state: vec![vec![Some(State::Full), Some(State::Empty)]],
+            out_state: vec![vec![Some(State::Empty), Some(State::Full)]],
         },
-        _ => (),
-    }
+        Rule::Linear {
+            in_state: vec![
+                vec![None, Some(State::Empty)],
+                vec![Some(State::Full), Some(State::Full)],
+            ],
+            out_state: vec![
+                vec![None, Some(State::Full)],
+                vec![Some(State::Empty), Some(State::Full)],
+            ],
+        },
+        Rule::Linear {
+            in_state: vec![
+                vec![Some(State::Full), Some(State::Full)],
+                vec![None, Some(State::Empty)],
+            ],
+            out_state: vec![
+                vec![Some(State::Empty), Some(State::Full)],
+                vec![None, Some(State::Full)],
+            ],
+        },
+    ]
 }
 
-// changes the state of the cell that was interacted with
-fn update_grid(win: &Rect, model: &mut Model, win_pos: &Point2) {
-    let w = win.x.len() / (GRID_SIZE as f32);
-    let h = win.y.len() / (GRID_SIZE as f32);
-    let x = ((win_pos.x - win.x.start) / w) as usize;
-    let y = ((win.y.end - win_pos.y) / h) as usize;
-    model.active[x][y] = match model.active[x][y] {
-        State::Full => State::Empty,
-        State::Empty => State::Full,
-    }
-}
-
-fn update(_app: &App, model: &mut Model, _update: Update) {
-    if model.last.elapsed().as_millis() < 50 {
-        return;
-    } else {
-        model.last = Instant::now();
-    }
-    let mut inactive = model.active.clone();
-    for (i, col) in model.active.iter().enumerate() {
-        for (j, cell) in col.iter().enumerate() {
-            for rule in model.rules.iter() {
-                match rule {
-                    Rule::Axial {
-                        in_state,
-                        out_state,
-                    } => model.axial((i, j), &mut inactive, in_state, out_state),
-                    Rule::Radial {
-                        current_state,
-                        surroundings,
-                        final_state,
-                    } => model.radial(
-                        &cell,
-                        (i, j),
-                        &mut inactive,
-                        current_state,
-                        surroundings,
-                        final_state,
-                    ),
-                }
-            }
-        }
-    }
-    model.active = inactive;
-}
-
-impl Model {
-    fn radial(
-        &self,
-        cell: &State,
-        cell_cords: (usize, usize),
-        inactive: &mut [[State; GRID_SIZE]; GRID_SIZE],
-        current_state: &State,
-        surroundings: &[(usize, State, Comparison)],
-        final_state: &State,
-    ) {
-        if current_state == cell {
-            let cells: Vec<_> = (-1i64..=1)
-                .flat_map(|ri| {
-                    (-1i64..=1).map(move |rj| (ri, rj)).map(|(ri, rj)| {
-                        self.active
-                            .get((cell_cords.0 as i64 + ri) as usize)
-                            .map(|col| col.get((cell_cords.1 as i64 + rj) as usize))
-                            .flatten()
-                    })
-                })
-                .filter_map(|cell| cell)
-                .collect();
-            if surroundings.iter().all(|(count, req_state, comp)| {
-                comp.compare(
-                    *count,
-                    cells.iter().filter(|val| val == &&req_state).count(),
-                )
-            }) {
-                inactive[cell_cords.0][cell_cords.1] = final_state.clone();
-            }
-        }
-    }
-
-    fn axial(
-        &self,
-        cell_cords: (usize, usize),
-        inactive: &mut [[State; GRID_SIZE]; GRID_SIZE],
-        in_state: &Vec<Vec<State>>,
-        out_state: &Vec<Vec<State>>,
-    ) {
-        // check all the states relative to the given cell
-        if in_state
-            .iter()
-            .enumerate()
-            .flat_map(|(ri, rule_col)| {
-                rule_col
-                    .iter()
-                    .enumerate()
-                    .map(move |(rj, rule_cell)| (ri, rj, rule_cell))
-            })
-            .all(|(ri, rj, rule_cell)| {
-                self.active.get(cell_cords.0 + ri).is_some_and(|col| {
-                    col.get(cell_cords.1 + rj)
-                        .is_some_and(|cell| cell == rule_cell)
-                })
-            })
-        {
-            // if cells match expected, perform the swaps to the new layout
-            for (ri, rj, state) in out_state.iter().enumerate().flat_map(|(ri, out_col)| {
-                out_col
-                    .iter()
-                    .enumerate()
-                    .map(move |(rj, state)| (ri, rj, state))
-            }) {
-                inactive[cell_cords.0 + ri][cell_cords.1 + rj] = state.clone();
-            }
-        }
-    }
-}
-
-fn view(app: &App, model: &Model, frame: Frame) {
-    let draw = app.draw();
-    let window = app.main_window();
-    let win = window.rect();
-    draw.background().color(BLUE);
-
-    draw_grid(&draw, &win, &model);
-
-    draw.to_frame(app, &frame).unwrap();
-}
-
-fn draw_grid(draw: &Draw, win: &Rect, model: &Model) {
-    let w = win.x.len() / (GRID_SIZE as f32);
-    let h = win.y.len() / (GRID_SIZE as f32);
-    let x0 = win.x.start + w / 2.;
-    let y0 = win.y.end - h / 2.;
-    for (i, col) in model.active.iter().enumerate() {
-        for (j, cell) in col.iter().enumerate() {
-            draw.rect()
-                .x_y(x0 + (i as f32) * w, y0 - (j as f32) * h)
-                .w_h(w, h)
-                .stroke_weight(0.5)
-                .stroke(GRAY)
-                .color(match cell {
-                    State::Full => WHITE,
-                    State::Empty => BLACK,
-                });
-        }
-    }
+fn game_of_life() -> Vec<Rule> {
+    vec![
+        Rule::Radial {
+            current_state: State::Empty,
+            surroundings: vec![(State::Full, Comparison::Equal(3))],
+            final_state: State::Full,
+        },
+        Rule::Radial {
+            current_state: State::Full,
+            surroundings: vec![(State::Full, Comparison::LessThan(2))],
+            final_state: State::Empty,
+        },
+        Rule::Radial {
+            current_state: State::Full,
+            surroundings: vec![(State::Full, Comparison::GreaterThan(3))],
+            final_state: State::Empty,
+        },
+    ]
 }
